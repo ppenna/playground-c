dd1d27ef6681018928f53645a814454ac2bff132
Pedro Henrique Penna <pedrohenriquepenna@gmail.com>
2023-08-16 07:36:24 -0300

    Structures

---
+++ 02-structs/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_NAME_LENGTH 64
+
+struct person
+{
+	char name[MAX_NAME_LENGTH];
+	int age;
+};
+
+int main(int argc, char *argv[])
+{
+	struct person p;
+
+	strcpy(p.name, "Pedro");
+	p.age = 31;
+	
+	printf("Hello, I am %s and I am %d \n", p.name, p.age);
+
+	return (0);
+}
